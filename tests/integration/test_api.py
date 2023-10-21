a9809035212eede4c1c61690ad309b519365c0dd
inesz <agnieszka.garsta@gmil.com>
2023-10-21 19:47:39 +0200

    Add api test

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -4,8 +4,14 @@ from flaskr import create_app
 
 app = create_app()
 
-def test_index_route():
+def test_hello():
     response = app.test_client().get('/hello')
 
     assert response.status_code == 200
     assert response.data.decode('utf-8') == 'Hello, World!'
+
+def test_tell_me_sth():
+    response = app.test_client().get('/tell-me-sth')
+
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == '{"are_you_sure":"GET"}\n'
